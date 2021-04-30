c3937c939b9d221dadc205797bcc6768ea7c8c12
andersj234 <andersanjoseminas@gmail.com>
2021-04-30 20:41:07 +0200

    cambios ejercicios 2

---
+++ ej2.js
@@ -0,0 +1,5 @@
+fetch("https://randomuser.me/api/").then(function recogeRespuesta(respuesta){
+    return respuesta.json();
+}).then(function recibeRespuesta(datos){
+    for(let i = 0; i <datos.results[i].; i++)
+}) eguoqnuogqujo
