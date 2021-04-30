4774bafe45f10c5eb8a4aba10cc6e97b96853b35
andersj234 <andersanjoseminas@gmail.com>
2021-04-30 20:42:30 +0200

    arreglo

--- ej2.js
+++ ej2.js
@@ -2,4 +2,4 @@ fetch("https://randomuser.me/api/").then(function recogeRespuesta(respuesta){
     return respuesta.json();
 }).then(function recibeRespuesta(datos){
     for(let i = 0; i <datos.results[i].; i++)
-}) eguoqnuogqujo
+}) 
